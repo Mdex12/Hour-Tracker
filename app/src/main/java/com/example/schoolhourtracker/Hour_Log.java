6f6e398799aea2961c6f33f9f89734bd1b72f08a
Mdex12 <65144119+Mdex12@users.noreply.github.com>
2020-06-07 08:48:08 -0600

    redo

---
+++ app/src/main/java/com/example/schoolhourtracker/Hour_Log.java
@@ -0,0 +1,15 @@
+package com.example.schoolhourtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hour_Log extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hour__log);
+    }
+}
